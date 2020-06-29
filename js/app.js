b53a61e04c6692ec49186dfae1c46ac98049ab88
ahmed fawzy <42780497+fawzy2010eng@users.noreply.github.com>
2020-06-29 13:01:01 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,12 +15,26 @@ var flipcard = document.querySelector('.flip-card');
 var innercard = document.querySelector('.flip-card-inner');
 var timer = document.querySelector('.ml-5 .timer');
 var pause = document.querySelector('.ml-5 .pause');
-var start = document.querySelector('.ml-5 .start')
-//setting the timer in the front card
+var start = document.querySelector('.ml-5 .start');
 
+//giving index for the input
+function indexingInputs(){
+	var inputs = document.querySelectorAll('input');
+
+	var rows = ['A' , 'B' , 'C' , 'D' , 'E' , 'F' , 'G' , 'H' , 'I'];
+	for(var k = 0; k < inputs.length; k++){
+		var column = (k)%9;
+		var row = rows[(((k) - (k)%9) / 9)];
+		inputs[k].setAttribute("data-index" , `${row}${column+1}`)
+	}
+
+}
+indexingInputs();
+
+//setting the timer in the front card
 var d = new Date();
 var n = d.getTime();
-function updatetimer(){
+function updateTimer(){
     var currenttime = new Date().getTime() - n ;
     var minute = 0;
     var sec = 0;
@@ -34,20 +48,22 @@ function updatetimer(){
     if(sec < 10){sec = `0${sec}`};
     timer.innerHTML = `${minute} : ${sec}`
 }
-//var myvar = setInterval(updatetimer , 1000);
+//var myvar = setInterval(updateTimer , 1000);
 
 
 //setting the pause button
 
 pause.addEventListener('click',function(){
-    //    flip the grid
-    innercard.style.transform = 'rotateY(180deg)';
-    //    pause the timer
-    clearInterval(myvar);
-    //    disaple this button
-    this.disabled = true;
-    //    able the start button
-    start.disabled = false;
+//    //    flip the grid
+//    innercard.style.transform = 'rotateY(180deg)';
+//    //    pause the timer
+//    clearInterval(myvar);
+//    //    disaple this button
+//    this.disabled = true;
+//    //    able the start button
+//    start.disabled = false;
+	var conflicts = sudoku.getConflicts({ A1: 5, A2:5});
+	console.log(conflicts)
 })
 
 //setting the start button
@@ -71,204 +87,50 @@ start.addEventListener('click',function(){
         if(sec < 10){sec = `0${sec}`};
         timer.innerHTML = `${minute} : ${sec}`
     },1000)
+	//flip the grid
     innercard.style.transform = 'rotateY(0)';
-    this.disabled = true;
+    //disable this card
+	this.disabled = true;
+	//enable pause button
     pause.disabled = false;
-     paused = true;
-    
 })
 
-//creating grid function creategird(){
-	var count = 0;
-    //giving the inputs Is and Js
-    var inputs = document.querySelectorAll('input');
-
-    for(var j = 0; j < 81; j++){
-        inputs[j].setAttribute('data-j' , Math.floor(j/9));    
-    }
-    for(var i = 0; i < 81; i++){
-        inputs[i].setAttribute('data-i' , Math.floor(i%9))
-
-    }
-
-    var idol = [1,2,3,4,5,6,7,8,9];
-
-    //creating full array
-    let fullArray = Array(9).fill().map(() => Array(9).fill(0));
-    
-	for(var k = 0; k < inputs.length; k++){
-		console.log(k);
-		var cellJ = k - (k - (Math.floor(k%9)));
-        var cellI = (k - (k%9))/9;
-		// loop through the ith array
-        for(var i = 0; i < 9; i++){
-            if(idol.indexOf(fullArray[cellI][i]) != -1){
-                idol[idol.indexOf(fullArray[cellI][i])] = ''  
-            }  
-        }
-		console.log(idol);
-		//	looping through jth array
-		for(var j = 0; j < 9; j++){
-            if(idol.indexOf(fullArray[j][cellJ]) != -1){
-                idol[idol.indexOf(fullArray[j][cellJ])] = ''  
-            }  
-        }
-				console.log(idol);
-
-		//creating suare array
-		var square = [];
-		
-		//choosing the square for the cell
-		if(cellI >= 0 && cellI < 3){
-            if(cellJ >= 0 && cellJ < 3){
-                square.push(fullArray[0][0]);
-                square.push(fullArray[1][0]);
-                square.push(fullArray[2][0]);
-                square.push(fullArray[0][1]);
-                square.push(fullArray[1][1]);
-                square.push(fullArray[2][1]);
-                square.push(fullArray[0][2]);
-                square.push(fullArray[1][2]);
-                square.push(fullArray[2][2]);
-                
-            }else if(cellJ >= 3 && cellJ < 6){
-                
-                square.push(fullArray[0][3]);
-                square.push(fullArray[1][3]);
-                square.push(fullArray[2][3]);
-                square.push(fullArray[0][4]);
-                square.push(fullArray[1][4]);
-                square.push(fullArray[2][4]);
-                square.push(fullArray[0][5]);
-                square.push(fullArray[1][5]);
-                square.push(fullArray[2][5]);
-                
-            }else{
-                
-                
-                square.push(fullArray[0][6]);
-                square.push(fullArray[1][6]);
-                square.push(fullArray[2][6]);
-                square.push(fullArray[0][7]);
-                square.push(fullArray[1][7]);
-                square.push(fullArray[2][7]);
-                square.push(fullArray[0][8]);
-                square.push(fullArray[1][8]);
-                square.push(fullArray[2][8]);
-                
-            }
-            
-        }
-        else if(cellI >= 3 && cellI < 6){
-            if(cellJ >= 0 && cellJ < 3){
-                
-                square.push(fullArray[3][0]);
-                square.push(fullArray[4][0]);
-                square.push(fullArray[5][0]);
-                square.push(fullArray[3][1]);
-                square.push(fullArray[4][1]);
-                square.push(fullArray[5][1]);
-                square.push(fullArray[3][2]);
-                square.push(fullArray[4][2]);
-                square.push(fullArray[5][2]);
-                
-            }else if(cellJ >= 3 && cellJ < 6){
-                
-                
-                square.push(fullArray[3][3]);
-                square.push(fullArray[4][3]);
-                square.push(fullArray[5][3]);
-                square.push(fullArray[3][4]);
-                square.push(fullArray[4][4]);
-                square.push(fullArray[5][4]);
-                square.push(fullArray[3][5]);
-                square.push(fullArray[4][5]);
-                square.push(fullArray[5][5]);
-                
-            }else{
-                
-                
-                square.push(fullArray[3][6]);
-                square.push(fullArray[4][6]);
-                square.push(fullArray[5][6]);
-                square.push(fullArray[3][7]);
-                square.push(fullArray[4][7]);
-                square.push(fullArray[5][7]);
-                square.push(fullArray[3][8]);
-                square.push(fullArray[4][8]);
-                square.push(fullArray[5][8]);
-                
-            }
-            
-        }
-        else{
-            if(cellJ >= 0 && cellJ < 3){
-                
-                square.push(fullArray[6][0]);
-                square.push(fullArray[7][0]);
-                square.push(fullArray[8][0]);
-                square.push(fullArray[6][1]);
-                square.push(fullArray[7][1]);
-                square.push(fullArray[8][1]);
-                square.push(fullArray[6][2]);
-                square.push(fullArray[7][2]);
-                square.push(fullArray[8][2]);
-                
-            }else if(cellJ >= 3 && cellJ < 6){
-                
-                
-                square.push(fullArray[6][3]);
-                square.push(fullArray[7][3]);
-                square.push(fullArray[8][3]);
-                square.push(fullArray[6][4]);
-                square.push(fullArray[7][4]);
-                square.push(fullArray[8][4]);
-                square.push(fullArray[6][5]);
-                square.push(fullArray[7][5]);
-                square.push(fullArray[8][5]);
-                
-            }else{
-                
-                square.push(fullArray[6][6]);
-                square.push(fullArray[7][6]);
-                square.push(fullArray[8][6]);
-                square.push(fullArray[6][7]);
-                square.push(fullArray[7][7]);
-                square.push(fullArray[8][7]);
-                square.push(fullArray[6][8]);
-                square.push(fullArray[7][8]);
-                square.push(fullArray[8][8]);
-                
-            }
-            
-        }
-		//looping through square
-		for(var i = 0; i < 9; i++){
-			if(idol.indexOf(square[i]) != -1){
-				idol[idol.indexOf(square[i])] = ''
-			}
-		};
-		console.log(idol);
-
-		//clearing the idol from the ''s
-		var idolx = [];
-		for(var i = 0; i < 9; i++){
-			if(idol[i] != ''){
-				idolx.push(idol[i]);
-			}
-		}
-		console.log(idolx);
-		//get random item from the idol array
-		var rand = Math.floor((Math.random() * idolx.length));	
-		console.log(rand,idolx[rand]);
-		
-		fullArray[cellI][cellJ] = idolx[rand];
-		idol = [1,2,3,4,5,6,7,8,9]
-	}
-	console.log(fullArray);
-	
+//creating the grid function
+function createGrid(level){
+	var puzzle = Object.entries(sudoku.generate(level)) ;
+	return puzzle
 }
-creategird();
+
+//creating grid array
+var puzzle = createGrid("easy");
+
+//filling the grid with the digits function
+function fillingGrid(array){
+	//calling ll the inputs
+	var inputs = document.querySelectorAll('input');
+	var inputArr =[]; 
+	//adding the indexo of input into array
+	for(var i = 0; i < inputs.length; i++){
+		inputArr.push(inputs[i].getAttribute('data-index'))
+	}
+	
+	for(var i = 0; i < puzzle.length; i++){
+		inputs[inputArr.indexOf(array[i][0])].value = puzzle[i][1];
+		inputs[inputArr.indexOf(array[i][0])].disabled = true;
+		inputs[inputArr.indexOf(array[i][0])].style.backgroundColor = '#cbcbcb'
+	}	
+}
+
+//filling grid with puzzle for the first time
+fillingGrid(puzzle)
+
+
+//solving the puzzle
+
+//var solvedPuzzle = sudoku.solve(puzzle);
+//console.log(solvedPuzzle)
+
+
 
 
 
